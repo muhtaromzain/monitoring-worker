6908b56c0bda09439d89df06be6ed712d87879b9
muhtaromzain <muhtaromzain@gmail.com>
2024-06-07 23:10:04 +0700

    Update sql

--- sql/init.sql
+++ sql/init.sql
@@ -10,17 +10,18 @@ CREATE TABLE `customer_portal_temp` (
 DROP TABLE `customer_portal_headers`;
 CREATE TABLE `customer_portal_headers` (
 	`dt_code` VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-	`order_number` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+	`order_number` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
 	`customer_code` VARCHAR(10) NULL,
 	`warehouse_code` VARCHAR(10) NULL,
 	`term_code` VARCHAR(4) NULL,
-	`is_success` TINYINT(1) DEFAULT 0,
+	`is_submitted` TINYINT(1) DEFAULT 0,
 	`remarks` LONGTEXT NULL,
-	`send_timestamps` INT(11) UNSIGNED NOT NULL,
+	`send_timestamps` INT(11) UNSIGNED DEFAULT NULL,
 	`created_on` INT(11) UNSIGNED NOT NULL,
 	UNIQUE KEY `uk_customer_portal_header_dt_code_created_on` (`dt_code`, `created_on`) USING BTREE,
 	INDEX `ix_customer_portal_headers_dt_code` (`dt_code`) USING BTREE,
-	INDEX `ix_customer_portal_headers_order_number` (`order_number`) USING BTREE
+	INDEX `ix_customer_portal_headers_order_number` (`order_number`) USING BTREE,
+	INDEX `ix_customer_portal_headers_created_on` (`created_on`) USING BTREE
 );
 
 DROP TABLE `customer_portal`;
