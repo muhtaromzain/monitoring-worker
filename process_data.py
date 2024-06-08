5b4375eae0ca303b7035e3fc6c5f8f948bf76071
muhtaromzain <muhtaromzain@gmail.com>
2024-06-08 21:49:45 +0700

    Add new logic and flow process data

--- process_data.py
+++ process_data.py
@@ -7,9 +7,10 @@ import os
 from templates.txt import Txt as template
 from itertools import groupby
 from model.customer_portal import CustomerPortal
+import ntpath
 
 class ProcessData():
-    def readCsv(filePath: str):
+    def readCsv(filePath: str, dataHeader: list):
         data = []
         if (filePath):
             with open(filePath) as csvfile:
@@ -17,10 +18,26 @@ class ProcessData():
                 for row in reader:
                     data.append({
                             'dt_code': row['DT code'],
-                            'order_number': row['Order number'],
-                            'barcode': row['Barcode'],
+                            'order_number': dataHeader[row['DT code']],
+                            'sku': row['SKU'],
                             'qty': row['Qty'],
-                            'created_on': int(datetime.now().timestamp())
+                            # 'created_on': int(datetime.now().timestamp())
+                            'created_on': row['Timestamps']
+                    })
+
+        return data
+
+    def insertTemp(filePath: str):
+        data = []
+        if (filePath):
+            with open(filePath) as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    data.append({
+                            'dt_code': row['DT code'],
+                            'sku': row['SKU'],
+                            'qty': row['Qty'],
+                            'created_on': row['Timestamps']
                     })
 
         return data
@@ -29,10 +46,10 @@ class ProcessData():
         dataForSql = []
         if (isDetail):
             for val in data:
-                dataForSql.append((val['dt_code'], val['order_number'], val['barcode'], val['qty'], val['created_on']))
+                dataForSql.append((val['dt_code'], val['order_number'], val['sku'], val['qty'], val['created_on']))
         else:
             for val in data:
-                dataForSql.append((val['dt_code'], val['order_number'], val['customer_code'], val['warehouse_code'], val['term_code'], val['created_on']))
+                dataForSql.append((val['dt_code'], val['order_number'], val['customer_code'], val['warehouse_code'], val['term_code'], val['is_submitted'], val['remarks'], val['send_timestamps'], val['created_on']))
 
 
         return dataForSql
@@ -53,23 +70,25 @@ class ProcessData():
         return header
         
     def moveFile(filePath: str):
-        listFilePath  = filePath.split('\\')
+        fileName = ntpath.basename(filePath)
         directoryName = datetime.today().strftime("%d-%m-%Y")
         directory     = './report/' + directoryName
 
-        destinationPath = directory + '/' + listFilePath[1] 
+        destinationPath = directory + '/' + fileName
 
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-        shutil.move(filePath, destinationPath)
+        process = shutil.move(filePath, destinationPath)
+        return process 
 
-    def export(data: list):
+    def export(data: list, isAuto = True):
         counter = 0
 
         directoryName = datetime.today().strftime('%d-%m-%Y')
         directory     = './output/' + directoryName
-        extFileName   = datetime.today().strftime('%Y%m%d')
+        # extFileName   = datetime.today().strftime('%Y%m%d')
+        extFileName   = data['basename'] + '_' + data['timestamps']
         fileName      = 'output_' + extFileName + '.txt'
 
         if not os.path.exists(directory):
@@ -77,9 +96,9 @@ class ProcessData():
 
         outputPath = directory + '/' + fileName 
 
-        CTL = CustomerPortal.GetCTL(data)
-        HDR = CustomerPortal.GetHeader(data)
-        LIN = CustomerPortal.GetDetail(data)
+        CTL = CustomerPortal.GetCTL(data, isAuto)
+        HDR = CustomerPortal.GetHeader(data, isAuto)
+        LIN = CustomerPortal.GetDetail(data, isAuto)
 
         bindedData = ProcessData.bindHeaderAndDetail(HDR, LIN)
 
@@ -112,3 +131,61 @@ class ProcessData():
         
         return result
     
+    def reservedOrderNumber(filePath: str) -> dict:
+        res = {}
+        with open(filePath) as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            data = list(reader)
+
+        data.pop(0)
+
+        flattenedDtCode = [item[0] for item in data]
+        distinctDtCode  = list(set(flattenedDtCode))
+        
+        flattenedTimestamps = [item[3] for item in data]
+        timestamps  = list(set(flattenedTimestamps))[0]
+
+        whereClauseDtCode = '(' + ', '.join(['"' + item + '"' for item in distinctDtCode]) + ')'
+
+        # delete temp
+        CustomerPortal.DeleteTemp(whereClauseDtCode, timestamps)
+
+        dataOrderNumber = CustomerPortal.GetHeaderOrderNumberCounter(distinctDtCode)
+
+        # converted dt order number
+        dtOrderNumber = {}
+        insertReservedOrderNumber = []
+        for val in dataOrderNumber:
+            value = list(val)
+            value.append(timestamps)
+            insertReservedOrderNumber.append(tuple(value))
+            dtOrderNumber[val[0]] = val[1]
+
+        # mapped null dt in epo
+        for dtCode in distinctDtCode:
+            if dtCode not in dtOrderNumber.keys():
+                dtOrderNumber[dtCode] = '00000'
+                # include DT code that are not in database
+                insertReservedOrderNumber.append((dtCode, '00000', '0000', '0000', '00', 0, None, 0, timestamps))
+
+        insertHeader = CustomerPortal.InsertHeader(insertReservedOrderNumber)
+        # insertHeader = True
+
+        # set data master
+        res['data'] = dtOrderNumber
+        res['detail'] = {
+            'dtCodeList': distinctDtCode,
+            'dtCode': whereClauseDtCode,
+            'timestamps': timestamps,
+            'filepath': filePath,
+            'filename': ntpath.basename(filePath),
+            'basename': ntpath.basename(filePath).replace('.csv', ''),
+            'outputFilename': ntpath.basename(filePath).replace('.csv', '.txt')
+        }
+
+        if insertHeader:
+            res['isSuccess'] = True
+        else:
+            res['isSuccess'] = False
+        
+        return res
