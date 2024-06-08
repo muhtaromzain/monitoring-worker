783d87630a80151dc14b6db787ff807c16275c6c
muhtaromzain <muhtaromzain@gmail.com>
2024-06-08 21:49:55 +0700

    Add new logic and flow handler

--- handler/MyHandler.py
+++ handler/MyHandler.py
@@ -1,7 +1,8 @@
 from watchdog.events import FileSystemEventHandler
 from process_data import ProcessData
-from config.database import Database
 from model.customer_portal import CustomerPortal
+import logging
+import os
 
 class MyHandler(FileSystemEventHandler):
     # def on_modified(self, event):
@@ -13,12 +14,32 @@ class MyHandler(FileSystemEventHandler):
     #     print(f'event type: {event.event_type}  path : {event.src_path}')
 
     def on_created(self, event):
-        dataCsv       = ProcessData.readCsv(event.src_path)
-        convertData   = ProcessData.convertData(dataCsv)
-        header        = ProcessData.generateHeader(dataCsv)
-        convertHeader = ProcessData.convertData(header, False)
-        insertData    = CustomerPortal.Insert(convertHeader, convertData)
+        if event.src_path.endswith('.csv'):
+            orderData = ProcessData.reservedOrderNumber(event.src_path)
 
-        if (insertData):
-            ProcessData.moveFile(event.src_path)
-            ProcessData.export(convertHeader)
+            if orderData['isSuccess']:
+                dataCsv       = ProcessData.readCsv(event.src_path, orderData['data'])
+                convertData   = ProcessData.convertData(dataCsv)
+                # header        = ProcessData.generateHeader(dataCsv)
+                # convertHeader = ProcessData.convertData(header, False)
+                insertData    = CustomerPortal.InsertTemp(convertData)
+
+                if insertData:
+                    ProcessData.moveFile(event.src_path)
+                    insert = CustomerPortal.InsertFromTemp(orderData['detail'])
+                    if insert:
+                        CustomerPortal.DeleteTemp(orderData['detail']['dtCode'], orderData['detail']['timestamps'])
+                        CustomerPortal.DeleteUnknownDtCode(orderData['detail'])
+                        ProcessData.export(orderData['detail'])
+                        # pass
+                    else:
+                        CustomerPortal.RollbackHeader(orderData['detail'])
+
+                else:
+                    CustomerPortal.RollbackHeader(orderData['detail'])
+
+            else:
+                CustomerPortal.RollbackHeader(orderData['detail'])
+        else:
+            logging.warn("Invalid file type, must send csv file")
+            os.remove(event.src_path)
