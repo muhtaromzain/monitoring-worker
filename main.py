bbfc2982b89088bd27f980dfeae0ffb0b5773513
muhtaromzain <muhtaromzain@gmail.com>
2024-06-09 18:36:22 +0700

    Set main function to run monitoring and api simultaneously

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from wd import WD
+# from wd import WD
+import threading
+import subprocess
+
+def run_script(script_name):
+    subprocess.run(["python", script_name])
 
 if __name__ == "__main__":
-    WD.watch()
+    api_thread        = threading.Thread(target=run_script, args=("api.py",))
+    monitoring_thread = threading.Thread(target=run_script, args=("monitoring.py",))
 
+    api_thread.start()
+    monitoring_thread.start()
+
+    api_thread.join()
+    monitoring_thread.join()
