50c55efcb9763f335d773889e19f65a0a55bb6be
muhtaromzain <muhtaromzain@gmail.com>
2024-06-11 02:18:53 +0700

    Change print to logging

--- wd.py
+++ wd.py
@@ -24,7 +24,7 @@ class WD():
         
         try:
             while True:
-                print('Watching...')
+                logging.info('Watching...')
                 time.sleep(2)
         except KeyboardInterrupt:
             observer.stop()
